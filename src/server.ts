46ae42b5a7fb768d3e890e1de5530e6319d0aeac
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-08-22 03:19:39 +0530

    feat: integrated auth router

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import errorHandler from "@/common/middleware/errorHandler";
 import rateLimiter from "@/common/middleware/rateLimiter";
 import requestLogger from "@/common/middleware/requestLogger";
 import { env } from "@/common/utils/envConfig";
+import { authRouter } from "./api/auth/authRouter";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
@@ -28,6 +29,7 @@ app.use(requestLogger);
 
 // Routes
 app.use("/health-check", healthCheckRouter);
+app.use("/auth", authRouter);
 
 // Swagger UI
 app.use(openAPIRouter);
